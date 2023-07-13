265f69b5baa1d81840bcca2ab5d4a73d413ad423
Onuoha UI <ifechinyere@gmail.com>
2023-07-13 14:54:45 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
