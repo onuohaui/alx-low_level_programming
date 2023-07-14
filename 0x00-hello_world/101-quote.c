91e56aeaac449a93814e814b067dd7c930c8e6ae
Onuoha UI <ifechinyere@gmail.com>
2023-07-13 17:01:31 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - this is the main function
- *
- * Return: 0 always
- */
+ * main - this is main function
+ * 
+ * Return: always 0
+*/
 
 int main(void)
 {
-    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, str, sizeof(str));
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+	return (1);
 }
