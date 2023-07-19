ffa8fefa3e37472c6d73cbd056b470f4562e47b5
Onuoha UI <ifechinyere@gmail.com>
2023-07-19 11:25:32 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,33 @@
 #include "main.h"
-
 /**
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: The number to start printing from
- * Return: Always 0
+ * Reture: always 0
  */
 
 void print_to_98(int n)
+
 {
 	while (n != 98)
 	{
-		_putchar(n / 100 + '0');
+		if (n >= 100)
+		{
+			_putchar(n / 100 + '0');
+		}
+		else
+		{
+			_putchar(' ');
+			_putchar(' ');
+		}
+
 		_putchar((n / 10) % 10 + '0');
 		_putchar(n % 10 + '0');
+
 		_putchar(',');
 		_putchar(' ');
-		n = (n < 98) ? n + 1 : n - 1;
+		n++;
 	}
+
 	_putchar('9');
 	_putchar('8');
 	_putchar('\n');
