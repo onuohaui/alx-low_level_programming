ca6f8f84fe115124ab6822ee25152d37f9e84d63
Onuoha UI <ifechinyere@gmail.com>
2023-07-18 18:41:18 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_sign -Prints sign of a number
+ * print_sign - prints sign of a number
  * @n: function parameter
+ *
  * Return: 1, 0 or -1
  */
-
 int print_sign(int n)
 {
 	if (n > 0)
