20f594fbc6ca45cc24d43e480f06f85db71f5cea
Onuoha UI <ifechinyere@gmail.com>
2023-07-20 04:22:45 -0700

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,16 +11,12 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
-	if (a > b && a > c)
+
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
@@ -30,4 +26,3 @@ int largest_number(int a, int b, int c)
 	}
 	return (largest);
 }
-
