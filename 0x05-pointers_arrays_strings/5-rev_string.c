16ca0b9f0840efae0e5599351845abfec1638692
Onuoha UI <ifechinyere@gmail.com>
2023-07-24 14:00:40 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be reversed
+ */
+void rev_string(char *s)
+{
+	char temp;
+	int i, len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
