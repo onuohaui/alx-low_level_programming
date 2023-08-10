afc637d017c07e2d0735653f0432715f740066ac
Onuoha UI <ifechinyere@gmail.com>
2023-08-10 14:46:06 -0700

    Task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,129 +1,101 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
 
 /**
- * _putchar - Writes a character to stdout
- * @c: The character to print
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: On success 1, on error -1
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int _putchar(char c);
-
-/**
- * print_number - Prints an array of integers as a number
- * @arr: The array of integers to print
- * @size: The size of the array
- */
-void print_number(int *arr, int size)
+int is_digit(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < size; i++)
+	while (s[i])
 	{
-		_putchar(arr[i] + '0');
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
+	return (1);
 }
 
 /**
- * multiply - Multiplies two arrays of integers
- * @num1: The first array of integers
- * @size1: The size of the first array
- * @num2: The second array of integers
- * @size2: The size of the second array
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: A pointer to the resulting array
+ * Return: the length of the string
  */
-int *multiply(int *num1, int size1, int *num2, int size2)
+int _strlen(char *s)
 {
-	int i, j;
-	int *result;
+	int i = 0;
 
-	result = calloc(size1 + size2, sizeof(int));
-	if (result == NULL)
+	while (s[i] != '\0')
 	{
-		printf("Error\n");
-		exit(98);
+		i++;
 	}
-
-	for (i = size1 - 1; i >= 0; i--)
-	{
-		for (j = size2 - 1; j >= 0; j--)
-		{
-			result[i + j + 1] += num1[i] * num2[j];
-			result[i + j] += result[i + j + 1] / 10;
-			result[i + j + 1] %= 10;
-		}
-	}
-
-	return (result);
+	return (i);
 }
 
 /**
- * convert_to_array - Converts a string to an array of integers
- * @str: The string to convert
- * @size: A pointer to store the size of the array
- *
- * Return: A pointer to the resulting array
+ * errors - handles errors for main
  */
-int *convert_to_array(char *str, int *size)
+void errors(void)
 {
-	int i;
-	int *num;
-
-	for (i = 0; str[i]; i++)
-	{
-		if (str[i] < '0' || str[i] > '9')
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
-
-	*size = i;
-	num = malloc(sizeof(int) * (*size));
-
-	if (num == NULL)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (i = 0; i < *size; i++)
-	{
-		num[i] = str[i] - '0';
-	}
-
-	return (num);
+	printf("Error\n");
+	exit(98);
 }
 
 /**
- * main - Entry point of the program
- * @argc: The number of command line arguments
- * @argv: An array of strings containing the arguments
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0 on success, 98 on error
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int size1, size2;
-	int *num1, *num2, *result;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		return (98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	num1 = convert_to_array(argv[1], &size1);
-	num2 = convert_to_array(argv[2], &size2);
-
-	result = multiply(num1, size1, num2, size2);
-	print_number(result, size1 + size2);
-	printf("\n");
-
-	free(num1);
-	free(num2);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
 	free(result);
-
 	return (0);
 }
