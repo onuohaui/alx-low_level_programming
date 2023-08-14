19780ff112a23e2c68a0ab0d5d50f126efcd7598
Onuoha UI <ifechinyere@gmail.com>
2023-08-14 11:19:10 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog's basic details
+ * @name: First name
+ * @age: age
+ * @owner: Owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
