b673254a0bee2aa8c5f9402e670331c4cd73272a
Onuoha UI <ifechinyere@gmail.com>
2023-08-16 16:10:35 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - Prints a name using a function given as a parameter.
+ * @name: The name to print.
+ * @f: The function to use to print the name.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+  if (name == NULL || f == NULL)
+    return;
+
+  f(name);
+}
