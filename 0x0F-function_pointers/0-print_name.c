10ab3780bcc9b401a178d4275666f2aca47a2495
Onuoha UI <ifechinyere@gmail.com>
2023-08-16 16:28:35 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
  * print_name - Prints a name using a function given as a parameter.
@@ -9,5 +8,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-  f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
