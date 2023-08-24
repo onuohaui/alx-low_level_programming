3cef426be174486e3844aefdf27ba7a097d6c55d
Onuoha <ifechinyereonuoha@gmail.com>
2023-08-24 16:37:17 -0700

    first list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
+void before_main(void) __attribute__((constructor));
+
 /**
-  before_main - Prints the required strings before the main function executes.
-  */
-
-void before_main(void) __attribute__ ((constructor));
-
+ * before_main - Prints the required strings before the main function executes.
+ */
 void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
