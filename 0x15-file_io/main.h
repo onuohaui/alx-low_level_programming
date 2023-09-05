3f1214cdd6f1185ac4a517f9f43c9502163a4497
Onuoha <ifechinyereonuoha@gmail.com>
2023-09-04 17:10:09 -0700

    Task 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
