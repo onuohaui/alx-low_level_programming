5890471c68c34c61b6bff789fa615f64b4653a64
Onuoha UI <ifechinyereonuoha@gmail.com>
2023-10-02 15:02:36 -0700

    Task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,108 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int open_files(char *file_from, char *file_to);
+ssize_t copy_content(int fd_from, int fd_to);
+void close_files(int fd_from, int fd_to);
+
+/**
+ * main - function to copy content
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, error code on failure
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open_files(argv[1], argv[2]);
+	fd_to = fd_from % (1 << 16);
+	fd_from = fd_from / (1 << 16);
+
+	if (copy_content(fd_from, fd_to) == -1)
+		exit(98);
+
+	close_files(fd_from, fd_to);
+
+	return (0);
+}
+
+/**
+ * open_files - Open source/destination files
+ * @file_from: source file
+ * @file_to: destination file
+ * Return: Combined file descriptors or -1
+ */
+int open_files(char *file_from, char *file_to)
+{
+	int fd_from, fd_to;
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close(fd_from);
+		exit(99);
+	}
+
+	return (fd_from * (1 << 16) + fd_to);
+}
+
+/**
+ * copy_content - Copies content from fd_from to fd_to
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * Return: 0 on success or -1 on failure
+ */
+ssize_t copy_content(int fd_from, int fd_to)
+{
+	ssize_t read_count, write_count;
+	char buffer[1024];
+
+	while ((read_count = read(fd_from, buffer, 1024)) > 0)
+	{
+		write_count = write(fd_to, buffer, read_count);
+		if (write_count != read_count)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to\n");
+			return (-1);
+		}
+	}
+
+	return (read_count);
+}
+
+/**
+ * close_files - Closes the file descriptors
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ */
+void close_files(int fd_from, int fd_to)
+{
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+}
