3848f95ff094109b6a0448a6e6ccfbe6dc3d403c
Onuoha UI <ifechinyereonuoha@gmail.com>
2023-10-02 18:22:10 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,114 +3,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int open_files(char *file_from, char *file_to, int *fd_from, int *fd_to);
-ssize_t copy_content(int fd_from, int fd_to);
-void close_files(int fd_from, int fd_to);
+#define MAXSIZE 1024
+
+void error_exit(int error_code, char *file_name, int fd);
 
 /**
- * main - function to copy content
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success, error code on failure
+ * main - Copies the content of one file to another file.
+ * @argc: The number of command-line arguments.
+ * @argv: The command-line arguments.
+ * Return: 0 on success, or an error code on failure.
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
+	int file_in, file_out;
+	ssize_t read_stat, write_stat;
+	char buffer[MAXSIZE];
 
 	if (argc != 3)
+		error_exit(97, NULL, 0);
+
+	file_in = open(argv[1], O_RDONLY);
+	if (file_in == -1)
+		error_exit(98, argv[1], 0);
+
+	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_out == -1)
+		error_exit(99, argv[2], 0);
+
+	while ((read_stat = read(file_in, buffer, MAXSIZE)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		write_stat = write(file_out, buffer, read_stat);
+		if (write_stat != read_stat)
+			error_exit(99, argv[2], 0);
 	}
 
-	if (open_files(argv[1], argv[2], &fd_from, &fd_to) == -1)
-		return (1);
+	if (read_stat == -1)
+		error_exit(98, argv[1], 0);
 
-	if (copy_content(fd_from, fd_to) == -1)
-	{
-		close_files(fd_from, fd_to);
-		exit(98);
-	}
+	if (close(file_in) == -1)
+		error_exit(100, NULL, file_in);
 
-	close_files(fd_from, fd_to);
+	if (close(file_out) == -1)
+		error_exit(100, NULL, file_out);
 
 	return (0);
 }
 
 /**
- * open_files - Open source/destination files
- * @file_from: source file
- * @file_to: destination file
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- * Return: Combined file descriptors or -1
+ * error_exit - Handle errors by printing appropriate
+ *  error messages and exiting.
+ * @error_code: The error code that determines the error message.
+ * @file_name: The file name that the error refers to.
+ * @fd: The file descriptor that the error refers to.
  */
-int open_files(char *file_from, char *file_to, int *fd_from, int *fd_to)
+
+void error_exit(int error_code, char *file_name, int fd)
 {
-	*fd_from = open(file_from, O_RDONLY);
-	if (*fd_from == -1)
+	switch (error_code)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			break;
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
+			break;
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
+			break;
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+			break;
+		default:
+			return;
 	}
-
-	*fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (*fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		close(*fd_from);
-		exit(99);
-	}
-
-	return (0);
-}
-
-/**
- * copy_content - Copies content from fd_from to fd_to
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- * Return: 0 on success or -1 on failure
- */
-ssize_t copy_content(int fd_from, int fd_to)
-{
-	ssize_t read_count, write_count;
-	char buffer[1024];
-
-	while ((read_count = read(fd_from, buffer, 1024)) > 0)
-	{
-		write_count = write(fd_to, buffer, read_count);
-		if (write_count != read_count)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to\n");
-			return (-1);
-		}
-	}
-
-	if (read_count == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from\n");
-		return (-1);
-	}
-
-	return (0);
-}
-
-/**
- * close_files - Closes the file descriptors
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- */
-void close_files(int fd_from, int fd_to)
-{
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	exit(error_code);
 }
