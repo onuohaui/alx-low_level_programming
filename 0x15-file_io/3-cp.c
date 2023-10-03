e19c2d1a8962c2ac3d9ab7a6c8c94086f8174513
Onuoha UI <ifechinyereonuoha@gmail.com>
2023-10-02 17:09:49 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,19 +3,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * open_files - Open source/destination files
+ * @file_from: source file
+ * @file_to: destination file
+ *
+ * Return: Combined file descriptors or -1
+ */
 int open_files(char *file_from, char *file_to);
+
+/**
+ * copy_content - Copies content from fd_from to fd_to
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ *
+ * Return: 0 on success or -1 on failure
+ */
 ssize_t copy_content(int fd_from, int fd_to);
+
+/**
+ * close_files - Closes the file descriptors
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ */
 void close_files(int fd_from, int fd_to);
 
 /**
  * main - function to copy content
  * @argc: argument count
  * @argv: argument vector
+ *
  * Return: 0 on success, error code on failure
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
+	int fd_from = -1, fd_to = -1;
 
 	if (argc != 3)
 	{
@@ -24,11 +46,22 @@ int main(int argc, char *argv[])
 	}
 
 	fd_from = open_files(argv[1], argv[2]);
-	fd_to = fd_from % (1 << 16);
-	fd_from = fd_from / (1 << 16);
+
+	if (fd_from == -1)
+		exit(98);
+
+	fd_to = open_files(argv[1], argv[2]);
+	if (fd_to == -1)
+	{
+		close_files(fd_from, fd_to);
+		exit(98);
+	}
 
 	if (copy_content(fd_from, fd_to) == -1)
+	{
+		close_files(fd_from, fd_to);
 		exit(98);
+	}
 
 	close_files(fd_from, fd_to);
 
@@ -39,6 +72,7 @@ int main(int argc, char *argv[])
  * open_files - Open source/destination files
  * @file_from: source file
  * @file_to: destination file
+ *
  * Return: Combined file descriptors or -1
  */
 int open_files(char *file_from, char *file_to)
@@ -48,14 +82,14 @@ int open_files(char *file_from, char *file_to)
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		perror("Error: Can't read from file");
 		exit(98);
 	}
 
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		perror("Error: Can't write to");
 		close(fd_from);
 		exit(99);
 	}
@@ -67,6 +101,7 @@ int open_files(char *file_from, char *file_to)
  * copy_content - Copies content from fd_from to fd_to
  * @fd_from: source file descriptor
  * @fd_to: destination file descriptor
+ *
  * Return: 0 on success or -1 on failure
  */
 ssize_t copy_content(int fd_from, int fd_to)
@@ -79,18 +114,18 @@ ssize_t copy_content(int fd_from, int fd_to)
 		write_count = write(fd_to, buffer, read_count);
 		if (write_count != read_count)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to fd %d\n", fd_to);
+			perror("Error: Can't write to");
 			return (-1);
 		}
 	}
 
 	if (read_count == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from fd %d\n", fd_from);
+		perror("Error: Can't read from");
 		return (-1);
 	}
 
-	return (0);
+	return (read_count);
 }
 
 /**
@@ -102,13 +137,13 @@ void close_files(int fd_from, int fd_to)
 {
 	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		perror("Error: Can't close fd");
 		exit(100);
 	}
 
 	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		perror("Error: Can't close fd");
 		exit(100);
 	}
 }
