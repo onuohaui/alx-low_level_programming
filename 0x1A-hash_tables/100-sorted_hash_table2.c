5a8db59955ddc495ba3bbe4c399975fe3bcbc3fd
Onuoha UI <ifechinyereonuoha@gmail.com>
2024-02-02 18:34:29 -0800

    Task 100

---
+++ 0x1A-hash_tables/100-sorted_hash_table2.c
@@ -0,0 +1,80 @@
+#include "hash_tables.h"
+
+/**
+ * shash_table_print - function to print the key:value from ht
+ * @ht: pointer to hash table
+ *
+ */
+void shash_table_print(const shash_table_t *ht)
+{
+	shash_node_t *bucket;
+	int not_fin = 0;
+
+	if (!ht)
+		return;
+	bucket = ht->shead;
+	printf("{");
+	while (bucket)
+	{
+		if (not_fin)
+			printf(", ");
+		printf("'%s': '%s'", bucket->key, bucket->value);
+		not_fin = 1;
+		bucket = bucket->snext;
+	}
+	printf("}\n");
+}
+/**
+ * shash_table_print_rev - function to print the key:value from ht in reverse
+ * @ht: pointer to hash table
+ *
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	shash_node_t *bucket;
+	int not_fin = 0;
+
+	if (!ht)
+		return;
+	bucket = ht->stail;
+	printf("{");
+	while (bucket)
+	{
+		if (not_fin)
+			printf(", ");
+		printf("'%s': '%s'", bucket->key, bucket->value);
+		not_fin = 1;
+		bucket = bucket->sprev;
+	}
+	printf("}\n");
+}
+/**
+ * shash_table_delete - free hash table and all nodes
+ * @ht: pointer to hash table
+ *
+ */
+void shash_table_delete(shash_table_t *ht)
+{
+	shash_node_t *bucket, *aux_free;
+	unsigned long int i = 0;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		bucket = ht->array[i];
+		while (bucket)
+		{
+			aux_free = bucket;
+			bucket = bucket->next;
+			if (aux_free->key)
+				free(aux_free->key);
+			if (aux_free->value)
+				free(aux_free->value);
+			free(aux_free);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
